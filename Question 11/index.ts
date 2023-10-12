290e74b1116493b507a1fc1325038da5dca32bc9
aymanzaveri <147271833+aymanzaveri@users.noreply.github.com>
2023-10-12 13:07:41 +0500

    Delete Question 11 directory

--- Question 11/index.ts
+++
@@ -1,8 +0,0 @@
-// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
-let names1:string[]=["Ayman","Urooba","Ayyan"];
-let message1:string="You are invited to my place";
-console.log(names1[0]+" " + message1);
-console.log(names1[1]+" " + message1);
-console.log(names1[2]+" " + message1);
-
-
