2e82397cdde66b2d370031889b67803978b91c10
aymanzaveri <147271833+aymanzaveri@users.noreply.github.com>
2023-10-12 12:52:55 +0500

    Add files via upload

---
+++ Question 1/index.js
@@ -0,0 +1,4 @@
+//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+let firstname = "Ayman Tariq";
+console.log(`hello ${firstname}, would you like to learn some python today?`);
+export {};
