fd9bb2a6a3dd5df5466e7227343d14d7ca972fde
aymanzaveri <147271833+aymanzaveri@users.noreply.github.com>
2023-10-12 13:00:47 +0500

    Add files via upload

---
+++ Question 2/index.js
@@ -0,0 +1,6 @@
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
+let firstname1 = "Ayman Tariq";
+console.log(firstname1.toLowerCase());
+console.log(firstname1.toUpperCase());
+console.log(firstname1.charAt(0).toUpperCase() + firstname1.slice(1));
+export {};
