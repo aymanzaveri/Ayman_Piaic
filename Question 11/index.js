b980fa5923c674d963f5ae019667538757778ca7
aymanzaveri <147271833+aymanzaveri@users.noreply.github.com>
2023-10-12 13:06:37 +0500

    Add files via upload

---
+++ Question 11/index.js
@@ -0,0 +1,7 @@
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let names1 = ["Ayman", "Urooba", "Ayyan"];
+let message1 = "You are invited to my place";
+console.log(names1[0] + " " + message1);
+console.log(names1[1] + " " + message1);
+console.log(names1[2] + " " + message1);
+export {};
