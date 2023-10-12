29a0957e51a72313858ba52d84d64fe292020991
aymanzaveri <147271833+aymanzaveri@users.noreply.github.com>
2023-10-12 13:06:33 +0500

    Add files via upload

---
+++ Question 9/index.js
@@ -0,0 +1,7 @@
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+//have to store your favourite number as a variable and then show it in the terminal with a statement
+let num1 = 2023;
+console.log(`my favourite number is ${num1}`);
+//Store the quote which is your favorite into a variable and then it should be printed on the terminal with the author name
+console.log(`Adolf Hitler once said,"If you want to shine like a sun you have to burn like it"`);
+export {};
